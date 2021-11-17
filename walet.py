7810239c9711ac35d83bb45725aff1a51d79d202
Renato Martins <renatoroma@hotmail.com>
2021-11-17 09:38:45 -0300

    Atualização p criar relação entre as vendas e produtos

--- walet.py
+++ walet.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os,re
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -9,21 +9,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.dirname(os.path.abs
 db = SQLAlchemy(app)
 
 
+association_table = db.Table('association', db.Model.metadata,
+    db.Column('purchase_id', db.ForeignKey('purchase.id')),
+    db.Column('product_id', db.ForeignKey('product.id'))
+)
+
 
 class Purchase(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
     sold_at = db.Column(db.DateTime, nullable = False)
-    customer_name = db.Column(db.String(50), unique=True, nullable = False)
-    customer_doc = db.Column(db.String(11), unique=True, nullable=False, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable = False)
+    document = db.Column(db.String(11), unique=True, nullable=False)
     purchase_total = db.Column(db.Float, nullable=False)
 
+    def validate_doc_len(self):
+        if not re.match(r'\d{11}', cpf):
+            return False
+        return True
+
+    def validate_doc_digits(self):
+        numbers = [int(digit) for digit in self.customer_doc if digit.isdigit()]
+        sum_of_products = sum(a * b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[9] != expected_digit:
+            return False
+        sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+        expected_digit = (sum_of_products * 10 % 11) % 10
+        if numbers[10] != expected_digit:
+            return False
+        return True
+
+
+
 class Products(db.Model):
-    customer_doc = db.Column(db.String(11), unique=True, nullable=False, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(1), nullable=False)
-    value_un = db.Column(db.Float, nullable=False)
+    value = db.Column(db.Float, nullable=False)
     qty = db.Column(db.Integer, nullable=False)
+    purchase = db.relationship("Purchase",
+                    secondary=association_table)
 
     def product_total(self):
-        return self.value_un * self.qty
+        return self.value * self.qty
 
-    def to_json(self):
-        return {'id': self.id, 'nome': self.nome, 'cpf': self.cpf, 'email': self.email, 'product_total':self.product_total()}
+
